31c4dcca7d1ffca64b24a6e02e909d38d2caf725
JahmalBroderickOPHS <97230430+JahmalBroderickOPHS@users.noreply.github.com>
2022-03-02 14:43:56 +0000

    Add files via upload

---
+++ selection04.py
@@ -0,0 +1,6 @@
+favColour = str(input("What is your favourite colour? >>> "))
+if favColour == "red" or favColour == "Red" or favColour == "RED":
+    print("I like red too!")
+    
+else:
+    print("I don't like",favColour,".",favColour,"is for losers!")
