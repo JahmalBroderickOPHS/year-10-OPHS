a64634e989a4e92e65574d5e21425a582e5ac00e
JahmalBroderickOPHS <97230430+JahmalBroderickOPHS@users.noreply.github.com>
2022-05-19 12:18:24 +0100

    Add files via upload

---
+++ 2D array challenge 1.py
@@ -0,0 +1,6 @@
+MySpaceCompany = []
+print(MySpaceCompany)
+for i in range(3):
+    spaceVehicle = input("Enter a type of space vehicle ")
+    MySpaceCompany.append(spaceVehicle)
+print(MySpaceCompany)
