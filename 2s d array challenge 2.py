20d6a7d72bb15c497958bb020ed8b73de4953117
JahmalBroderickOPHS <97230430+JahmalBroderickOPHS@users.noreply.github.com>
2022-05-19 12:29:20 +0100

    Add files via upload

---
+++ 2s d array challenge 2.py
@@ -0,0 +1,7 @@
+myspacecompany=["Rocket pack","moon base","moon buggy"]
+print(myspacecompany)
+myspacecompany[0]="rich people going to mars"
+print(myspacecompany)
+myspacecompany[1]="rich people going to the moon"
+print(myspacecompany)
+    
